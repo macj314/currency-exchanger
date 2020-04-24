d634dc9fc5c07e42d01b50d3e625323434fd87ae
unknown <macjay314@gmail.com>
2020-04-24 15:31:04 -0700

    Create error messages for user input (or lack of)

--- src/main.js
+++ src/main.js
@@ -9,8 +9,19 @@ $(document).ready(function() {
     console.log("button press")
     const inputNum = parseInt($('#input-number').val());
     const inputCurrency = $('#input-currency').val();
-    // $('#location').val("");
     event.preventDefault();
+    if (inputNum.length === 0){
+      $('#error-display').append(`Please enter a number into the first field.`);
+    } else if (isNaN(inputNum) === true && inputNum.length > 0){
+      $('#error-display').append(`Please only enter a number into the first field.`);
+    } else {
+      $('#input-display').text("You entered $" + inputNum);
+    }
+    if (inputCurrency.length === 0){
+      $('#error-display').append(`Please enter your desired currency conversion into the second field. A list of convertable currencies are listed above.`);
+    } else {
+      $('#input-display').append(" and " + inputCurrency + " as your desired currency to convert to.");
+    }
     (async () => {
       let exchanger = new Exchange();
       const response = await exchanger.request();
