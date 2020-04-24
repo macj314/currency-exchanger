05412c09e9e9c80d3db0efc76d31f226f7d26fc4
unknown <macjay314@gmail.com>
2020-04-24 09:03:41 -0700

    Base structure of MVP

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,3 +1,16 @@
 export class Exchange{
-  
+  async convert(currency) {
+    try {
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/${currency}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch {
+      return false;
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import $ from 'jquery';
+import {Exchange} from './exchange-service';
+import $ from 'jquery';
+
+$(document).ready(function() {
+
+  $('#btn-convert').click(function() {
+    const currency = 'USD';
+    // $('#location').val("");
+
+    (async () => {
+      let exchanger = new Exchange();
+      const response = await exchanger.convert(currency);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        $('#output-display').text(`${currency} $ ${response.conversion_rates.USD}`);
+      } else {
+        $('#error-display').text(`There was an error handling your request.`);
+        $('#error-display').text(`Please check your inputs and try again!`);
+      }
+    }
+
+  });
+});
