218f5775938a5e58223f1429c183656b5263e53e
unknown <macjay314@gmail.com>
2020-04-24 10:55:09 -0700

    Attempt to require dotenv at the top of exchange-service.js

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -2,10 +2,10 @@ export class Exchange {
 
   async convert() {
     try {
-      
+      console.log(process.env);
       // let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/${currency}`);
-      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
-      // let response = await fetch('http://dinoipsum.herokuapp.com/api/?format=json');
+      // let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+      let response = await fetch('http://dinoipsum.herokuapp.com/api/?format=json');
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
